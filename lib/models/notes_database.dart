b96d975b3b0480bc7011545bbcaac4fdc5dcfbba
Moises6669 <moisesalex132@gmail.com>
2023-01-17 12:14:06 -0700

    ADD: querys with sqlite methods

--- lib/models/notes_database.dart
+++ lib/models/notes_database.dart
@@ -27,4 +27,34 @@ class NotesDatabase {
     return await database.insert(tableName, note.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace);
   }
+
+  Future<int> updateNote(Note note) async {
+    return await database.update(tableName, note.toMap(),
+        where: 'id = ?',
+        whereArgs: [note.id],
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<List<Map<String, dynamic>>> getAllNotes() async {
+    return await database.query(tableName);
+  }
+
+  Future<Map<String, dynamic>> getNotes(int id) async {
+    var result =
+        await database.query(tableName, where: 'id = ?', whereArgs: [id]);
+
+    if (result.length > 0) {
+      return result.first;
+    }
+
+    return null;
+  }
+
+  Future<int> deleteNote(int id) async {
+    return await database.delete(tableName, where: 'id = ?', whereArgs: [id]);
+  }
+
+  closeDatabase() async {
+    await database.close();
+  }
 }
